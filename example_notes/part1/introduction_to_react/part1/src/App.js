f40ad37ac3d4944ddfdcf5c26af769dcd7b74965
Quang Nguyen <nguyenhaiquang3@gmail.com>
2021-10-29 03:08:57 -0500

    update app.js

--- example_notes/part1/introduction_to_react/part1/src/App.js
+++ example_notes/part1/introduction_to_react/part1/src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const age = 10;
+  const name = 'Quang';
+  return (
+    <div>
+      <p>Hello</p>
+      <Hello name={name} test="temp" age={age + 15}/>
+      <Hello name="Emily" age={age + 11}/>
+    </div>
+  )
+}
 
 export default App;
