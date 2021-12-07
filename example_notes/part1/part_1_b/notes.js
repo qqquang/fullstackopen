a3113138d320819ceed4dfa8ae90f2104a1aba2b
Quang Nguyen <nguyenhaiquang3@gmail.com>
2021-12-06 20:52:41 -0600

    'this' and classes notes

--- example_notes/part1/part_1_b/notes.js
+++ example_notes/part1/part_1_b/notes.js
@@ -1,6 +1,7 @@
 const array = [1,2,3,4];
 array.push(5);
 console.log('array length: ', array.length);
+console.log("\n")
 
 /** 
  * forEach recieve a function 
@@ -8,6 +9,7 @@ console.log('array length: ', array.length);
 array.forEach((value, index, array)=> {
     console.log('value + 2 = ', value + 2, ' at index ', index, ' in array ',array);
 })
+console.log("\n")
 
 /** 
  * use concat for immutable
@@ -18,6 +20,8 @@ const concatArr2 = array2.concat(4);
 
 console.log(array2);
 console.log(concatArr2);
+console.log("\n")
+
 
 /** 
  * map - creates a new array from old array
@@ -31,6 +35,7 @@ const mapStrinArr3 = array3.map(value => {return ('<li>' + value + '</li>')})
 console.log(array3);
 console.log(mapArray3);
 console.log(mapStrinArr3);
+console.log("\n")
 
 
 /** 
@@ -43,6 +48,7 @@ const [first, second, ...rest] = arr4;
 
 console.log(first, second);
 console.log(rest);
+console.log("\n")
 
 
 /** 
@@ -59,3 +65,65 @@ const obj1 = {
 
 console.log(obj1.name);
 console.log(obj1['name']['last'])
+console.log("\n")
+
+
+/**
+ * This keyword
+ */
+ const arto = {
+    name: 'Quang Nguyen',
+    age: 35,
+    education: 'PhD',
+    greet: function() {
+      console.log('hello, my name is ' + this.name)
+    },
+    doAddition: function(a, b) {
+        console.log("doAddition: ", a + b)
+    },
+}
+  
+arto.greet()  // "hello, my name is Quang Nguyen" gets printed
+
+// this doesn't add +1
+arto.growOlderArrow = () => this.age += 1;
+
+//does add 1
+arto.growOlderFunc = function() {
+    this.age += 1;
+}
+console.log(arto);
+arto.growOlderArrow();
+console.log("arrow function: ", arto.age);
+arto.growOlderFunc();
+console.log("regular function: ", arto.age);
+
+arto.doAddition(1, 4)        // 5 is printed
+const referenceToAddition = arto.doAddition
+referenceToAddition(10, 15)   // 25 is printed
+
+arto.greet()       // "hello, my name is Arto Hellas" gets printed
+
+// calling the method through a reference, the method loses knowledge of what the original this was
+const referenceToGreet = arto.greet
+referenceToGreet() // prints "hello, my name is undefined"
+console.log('\n');
+
+/**
+ * Classes
+ */
+
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    greet() {
+        console.log('hello, my name is', this.name);
+    }
+}
+
+const emily = new Person('Emily', 21);
+const quang = new Person('Quang', 25);
+emily.greet();
+quang.greet();
